64d0cd2e80a37864d76de0f9de0192f8aeff1683
EC2 Default User <ec2-user@ip-172-31-22-204.us-east-2.compute.internal>
2018-02-28 21:50:26 +0000

    pages: home, about, and contact all working

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,13 @@
 class PagesController < ApplicationController
 
 def home
-
 end
 
 def about
-
 end
+
+def contact
+end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   root 'pages#home'
+  
+  get 'home', to: 'pages#home'
   get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+  
   
   
   resources :articles
